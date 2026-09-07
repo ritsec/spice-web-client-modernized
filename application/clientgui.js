9e3dc58defa541f04349a2d28b94fad1acc3729e
Stella <stella@stellasec.com>
2026-09-07 14:57:57 -0400

    Revert "Pin viewer to right of screen"
    This reverts commit 0381f4cdc881fa8147b31ca31686bf62e49aa147.

--- application/clientgui.js
+++ application/clientgui.js
@@ -298,7 +298,6 @@ wdi.ClientGui = $.spcExtend(wdi.EventObject.prototype, {
 			var evLayerWrapper = $('<div id="eventLayerWrapper"></div>').css({
 				position: 'absolute',
 				top: '0',
-				left: '0',
 			    width: '100%',
 			    height: '100%',
 			    zIndex: '0'
